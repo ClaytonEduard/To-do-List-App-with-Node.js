1a74ab48e64a5b0073995d508a2d37c66ee408bc
claytoneduard <clayton_eduard@hotmail.com>
2022-03-28 16:23:46 -0300

    Arquivo ejs e css criandos para layout

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 // iniciando o servidor
-const express =  require('express');
+const express = require('express');
 const app = express();
 
+//definindo css
+app.use("/static", express.static("public"))
 
 
+//rota
+app.get('/', (req, res) => {
+    res.render("todo.ejs")
+})
+
+app.set("view engine", "ejs")
 
 // definindo o numero da porta do servidor
-app.listen(8081, ()=>{
+app.listen(8081, () => {
     console.log('Servidor funcionando!')
 })

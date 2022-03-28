6b7f6ad45b83a2ff553669d251e32eced741fb44
claytoneduard <clayton_eduard@hotmail.com>
2022-03-28 17:33:15 -0300

    conexão com o banco de bando mongoose web criada

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
 // iniciando o servidor
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose')
+
+//conectando com o server online
+const dotenv = require('dotenv')
+dotenv.config()
+
 
 //definindo css
 app.use("/static", express.static("public"));
 
 app.use(express.urlencoded({extends:true}))
+
+//conectando com o mongoose
+mongoose.connect(process.env.DB_CONNECT,{userNewUrlParser:true},()=>{
+    console.log('Conectado com o banco')
+    // definindo o numero da porta do servidor local
+    app.listen(8081, () => {
+        console.log('Servidor funcionando!')
+    })
+})
+
+
+
+
 
 // configuração da views
 app.set("view engine", "ejs")
@@ -19,9 +38,4 @@ app.get('/', (req, res) => {
 //metodo post
 app.post('/',(req,res)=>{
     console.log(req.body);
-})
-
-// definindo o numero da porta do servidor
-app.listen(8081, () => {
-    console.log('Servidor funcionando!')
 })
